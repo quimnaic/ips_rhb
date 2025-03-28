e8370f0b5f4120e60c63233e65dbb7a491f2bfb5
Edwin Quimbay <34946874+quimnaic@users.noreply.github.com>
2025-03-28 06:47:12 -0500

    Guardado antes de Actualizar angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopnavComponent } from './components/topnav/topnav.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FooterComponent, SidenavComponent, TopnavComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  isLoggedIn = true; 
   title = 'Front';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PatientsComponent } from './patients/patients.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'patients', component: PatientsComponent}
+];
