80210bd0bfde96b14db2a2b83724f78e35774306
Edwin Quimbay <34946874+quimnaic@users.noreply.github.com>
2025-03-28 06:50:14 -0500

    Guardado antes de Actualizar angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,10 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,FooterComponent, SidenavComponent, TopnavComponent, CommonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, FooterComponent, SidenavComponent, TopnavComponent, CommonModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
   isLoggedIn = true; 
